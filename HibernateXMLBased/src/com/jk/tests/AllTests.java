dca08a4fffaf9a479d74ba1361c5e5e694babad0
kjamandl <kjamandl@gmail.com>
2015-08-17 16:35:49 +0530

    testsa1

--- HibernateXMLBased/src/com/jk/tests/AllTests.java
+++ HibernateXMLBased/src/com/jk/tests/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ Test_EmployeeDAO.class, Test_HibUtils.class, Test_UserDAO.class })
+@SuiteClasses({ Test_EmployeeDAO.class, Test_UserDAO.class })
 public class AllTests {
 
 }
